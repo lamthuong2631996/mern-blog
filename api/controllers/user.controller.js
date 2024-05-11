130f40074e359f95e109af76103578f9d9e02201
Thuong <lamthuong2631996@gmail.com>
2024-05-12 00:29:54 +0700

    Create a test API route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,8 @@
+export const test = (req, res) => {
+  res.json({ message: "API is working!" });
+};
+
+export const updateUser = async (req, res, next) => {};
+export const deleteUser = async (req, res, next) => {};
+export const getUser = async (req, res, next) => {};
+export const getUsers = async (req, res, next) => {};
--- api/index.js
+++ api/index.js
@@ -1,19 +1,18 @@
-import express from 'express'
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from './routes/user.route.js';
 dotenv.config();
-
-const app = express()
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
-  });
-
-  mongoose
+mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log('MongoDb is connected');
+    console.log("MongoDb is connected");
   })
   .catch((err) => {
     console.log(err);
   });
+const app = express();
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
+app.use("/api/user", userRoutes); 
