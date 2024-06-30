1bcde15f9e96133008bcefe9715e421257c92cb7
Thuong <lamthuong2631996@gmail.com>
2024-06-30 12:50:04 +0700

    Create and add the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,13 +8,14 @@ import { SignUp } from "./pages/SignUp";
 import { About } from "./pages/About";
 import { Header } from "./components/Header";
 // import Header from './components/Header';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 // import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 // import CreatePost from './pages/CreatePost';
 // import UpdatePost from './pages/UpdatePost';
 // import PostPage from './pages/PostPage';
 // import ScrollToTop from './components/ScrollToTop';
 import {Search} from './pages/Search';
+import FooterCom from "./components/Footer";
 
 export default function App() {
   return (
@@ -38,7 +39,7 @@ export default function App() {
         {/* <Route path="/projects" element={<Projects />} />
         <Route path="/post/:postSlug" element={<PostPage />} /> */}
       </Routes>
-      {/* <Footer /> */}
+     <Footer />
     </BrowserRouter>
   );
 }
