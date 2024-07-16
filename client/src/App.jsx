cedded6d3739c6ec630254c0124cf81f92bf54e0
Thuong <lamthuong2631996@gmail.com>
2024-07-17 00:56:08 +0700

    Make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,15 +7,15 @@ import { Dashboard } from "./pages/Dashboard";
 import { SignUp } from "./pages/SignUp";
 import { About } from "./pages/About";
 import { Header } from "./components/Header";
-// import Header from './components/Header';
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
 // import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 // import CreatePost from './pages/CreatePost';
 // import UpdatePost from './pages/UpdatePost';
 // import PostPage from './pages/PostPage';
 // import ScrollToTop from './components/ScrollToTop';
-import {Search} from './pages/Search';
+import { Search } from "./pages/Search";
 import FooterCom from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -28,10 +28,10 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/search" element={<Search />} />
-        {/* <Route element={<PrivateRoute />}>
+        <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
-        <Route element={<OnlyAdminPrivateRoute />}>
+        {/* <Route element={<OnlyAdminPrivateRoute />}>
           <Route path="/create-post" element={<CreatePost />} />
           <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route> */}
@@ -39,7 +39,7 @@ export default function App() {
         {/* <Route path="/projects" element={<Projects />} />
         <Route path="/post/:postSlug" element={<PostPage />} /> */}
       </Routes>
-     <Footer />
+      <Footer />
     </BrowserRouter>
   );
 }
