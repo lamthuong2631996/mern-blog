5433bd4562c494deb089132e055acb17e6d3f785
Thuong <lamthuong2631996@gmail.com>
2024-07-22 23:42:43 +0700

    Add update user API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,16 +3,16 @@ import {
   deleteUser,
   getUser,
   getUsers,
-  // signout,
+  signout,
   updateUser,
   test,
 } from '../controllers/user.controller.js';
-// import { verifyToken } from '../utils/verifyUser.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test);
-// router.put('/update/:userId', verifyToken, updateUser);
+router.put('/update/:userId', verifyToken, updateUser);
 // router.delete('/delete/:userId', verifyToken, deleteUser);
 // router.post('/signout', signout);
 // router.get('/getusers', verifyToken, getUsers);
