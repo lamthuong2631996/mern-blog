4140e13953159d6e6625ef48d25b1f49b2db487f
Thuong <lamthuong2631996@gmail.com>
2024-07-23 13:25:04 +0700

    Add signout functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -61,14 +61,14 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 export const signout = (req, res, next) => {
-  // try {
-  //   res
-  //     .clearCookie('access_token')
-  //     .status(200)
-  //     .json('User has been signed out');
-  // } catch (error) {
-  //   next(error);
-  // }
+  try {
+    res
+      .clearCookie('access_token')
+      .status(200)
+      .json('User has been signed out');
+  } catch (error) {
+    next(error);
+  }
 };
 export const getUser = async (req, res, next) => {};
 export const getUsers = async (req, res, next) => {};
