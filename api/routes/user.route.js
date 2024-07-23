07b0cd1ff8ad922d154a515bf9f2efb9bffeac22
Thuong <lamthuong2631996@gmail.com>
2024-07-23 12:02:08 +0700

    Add delete user API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
-// router.delete('/delete/:userId', verifyToken, deleteUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
 // router.post('/signout', signout);
 // router.get('/getusers', verifyToken, getUsers);
 // router.get('/:userId', getUser);
